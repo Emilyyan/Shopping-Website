d42370736e6cfee1090e9c354f75d209be8b4436
Emilyyan <xw324@mail.missouri.edu>
2018-04-29 13:36:47 -0600

    update dashboard logic

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
 import { MatPaginator} from '@angular/material';
 import { MatTableDataSource } from '@angular/material/table';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,22 +12,40 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class DashboardComponent implements OnInit {
   //user: User;
-  user =[];
+  user = [];
+  selectedState = "";
+  localAddress = [];
+  formGroup1: FormGroup;//for saved address tab
+  formGroup2: FormGroup;//for userinfo tab
   displayedColumns = ['position', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource<any>();
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private httpClient:HttpClient){  }
+  constructor(private httpClient:HttpClient, private _formBuilder: FormBuilder){  }
 
   ngOnInit() {
+    this.formGroup1 = this._formBuilder.group({
+      state: ['', Validators.required],
+      city: ['', Validators.required],
+      address1: ['', Validators.required],
+      zip: ['', Validators.required],
+      phone: ['', Validators.required]
+    });
+
+    this.formGroup2 = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required]
+    });
     //id should be replaced by the return value from server side
+    //get user info for info and address tabs
     this.httpClient.get(`https://my-json-server.typicode.com/Emilyyan/Shopping-Website/Users?id=1`)
     .subscribe(
       (data:any[]) => {
         if(data.length) {
-          console.log(data);
+          console.log(data[0]);
           this.user = data[0];
-          
+          this.localAddress  = data[0].address;
+          this.selectedState = data[0].address.state;
         }
       }, 
       error => console.log("Error", error)
@@ -34,12 +53,13 @@ export class DashboardComponent implements OnInit {
     );
 
     //order history should be returned from server side after authentication
+    //get order info for order history tab
     this.httpClient.get(`https://my-json-server.typicode.com/Emilyyan/Shopping-Website/Orders`)
     .subscribe(
       (data:any[]) => {
         if(data.length) {
           this.dataSource.data = data;
-          console.log(this.dataSource.data);
+          //console.log(this.dataSource.data);
         }
       }, 
       error => console.log("Error", error)
@@ -48,41 +68,43 @@ export class DashboardComponent implements OnInit {
 
   }
 
-
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
 
-  update = function(user) {
+  //update user info tab
+  update = function(userinfo) {
+    if(this.formGroup2.valid){
     //below link if only for testing front end logic
-    //this.httpClient.post(`https://my-json-server.typicode.com/Emilyyan/Shopping-Website/Users`, {email: user.email, first_name: user.firstName, last_name: user.lastName})
-     this.httpClient.post(`/register-users`, {email: user.email, first_name: user.firstName, last_name: user.lastName})
-      .subscribe(
-        res => console.log(res)
-      );
-  } 
+      this.httpClient.post(`https://my-json-server.typicode.com/Emilyyan/Shopping-Website/Users`, {email: userinfo.email, first_name: userinfo.firstName, last_name: userinfo.lastName})
+     //this.httpClient.post(`/register-users`, {email: user.email, first_name: user.firstName, last_name: user.lastName})
+        .subscribe(
+          res => console.log(res)
+        )
+    }
+  }
+
+  //update saved address info tab
+  updateAddress = function(changedAddress) {
+    //below link if only for testing front end logic
+    if(this.formGroup1.valid){
+      this.httpClient.post(`https://my-json-server.typicode.com/Emilyyan/Shopping-Website/Users`, {changedAddress})
+      //this.httpClient.post(`/register-users`, {email: user.email, first_name: user.firstName, last_name: user.lastName})
+        .subscribe(
+          res => console.log(res)
+        )
+    }
+  }
+
+
+  states = [
+    'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware',
+    'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
+    'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+    'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico',
+    'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+    'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+  ];
 
 }
-/*
-export class User{
-  email: String;
-  firstName: String;
-  lastName: String;
-  address: String;
-}*/
-/*
-export interface Element {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: Element[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'}
-];*/
