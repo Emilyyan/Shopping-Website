7b82ba0d1a8a2d8ff95b9dd697aa8875d332192f
Emilyyan <xw324@mail.missouri.edu>
2018-05-02 15:53:09 -0600

    fix product detail page

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { HttpClient } from '@angular/common/http';
+import { Product } from '../interfaces';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,19 +9,21 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-
+  product: Product;
   constructor(private httpClient:HttpClient, @Inject(MAT_DIALOG_DATA) public data: any) { }
  
 
   ngOnInit() {
    this.httpClient.get(`https://my-json-server.typicode.com/Emilyyan/Shopping-Website/Products/?id=${this.data.PID}`)
     .subscribe(
-      (userInfo:any[]) => {
-        if(userInfo.length) {
-          console.log(userInfo[0]);
+      (productInfo:any[]) => {
+        if(productInfo.length) {
+          this.product = productInfo[0];
+          console.log(this.product.name);
         }
       }
-    )
+    ),
+    error => console.log("Error", error)
 
   }
 
