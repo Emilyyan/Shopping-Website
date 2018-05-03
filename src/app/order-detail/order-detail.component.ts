828ce10917aa73c940046255e209245641afec31
Emilyyan <xw324@mail.missouri.edu>
2018-05-02 18:27:25 -0600

    update orderDetail logic

--- src/app/order-detail/order-detail.component.ts
+++ src/app/order-detail/order-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { HttpClient } from '@angular/common/http';
 import { MAT_DIALOG_DATA } from '@angular/material';
-import { Order } from '../interfaces';
+import { Order, CartItem , Product} from '../interfaces';
 
 
 @Component({
@@ -12,6 +12,9 @@ import { Order } from '../interfaces';
 })
 export class OrderDetailComponent implements OnInit {
   order: Order;
+  products = [];
+  productArray = [];
+
 
   constructor(private httpClient:HttpClient, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
@@ -21,12 +24,50 @@ export class OrderDetailComponent implements OnInit {
       (orderInfo:any[]) => {
         if(orderInfo.length) {
           this.order = orderInfo[0];
-          console.log(this.order);
+          //console.log(this.order);
         }
-      }
+      },
+      error => console.log("Error", error)
     ), 
-    error => console.log("Error", error)
 
+    this.httpClient.get(`https://my-json-server.typicode.com/Emilyyan/Shopping-Website/Products`)
+    .subscribe(
+      (data:any[]) => {
+        if(data.length){
+          this.products = data;
+          //console.log(this.products);
+        }
+      },
+      error => console.log("Error", error),
+      () => {
+        for(let p of this.order.products){
+          let obj = this.getProductDetail(p.pid);
+          let orderitem: CartItem = {
+            pid: p.pid,
+            quantity: p.quantity,
+            name: obj.name,
+            price: obj.price
+          }
+
+          this.productArray.push(orderitem);
+        }
+      
+        //console.log(this.productArray);
+        console.log(this.productArray);
+      }
+    )
+  
   }
 
+  private getProductDetail(pid: number){
+    /*for(let i in this.products.id){
+      if(i === pid){
+
+      }
+    }*/
+    let index = this.products.findIndex(x => x.id === pid);
+    return this.products[index];
+  }
 }
+
+
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -3,7 +3,6 @@ import { MatDialog } from '@angular/material';
 import { FormControl, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -12,6 +11,7 @@ import { HttpClient } from '@angular/common/http';
 export class SignInComponent{
   hide = true;
   email = new FormControl('', [Validators.required, Validators.email]);
+  localRes;
 
   getErrorMessage() {
     return this.email.hasError('required') ? 'You must enter a value' :
@@ -25,18 +25,25 @@ export class SignInComponent{
   constructor(private httpClient:HttpClient){  }
 
   onSubmit = function(user) {
-    this.httpClient.get(`/get-users`)
+    this.httpClient.get(`/get-users`,{observe: 'response'})
     .subscribe(
-      (data:any[]) => {
+      (data) => {
+        /*
         if(data.length) {
           console.log(user.password);
           this.firstName = data[0].firstName;
           this.lasttName = data[0].lastName;
           console.log(this.firstName+this.lastName);
-        }
+        }*/
+        this.firstName = data.body[0].firstName;
+        this.lasttName = data.body[0].lastName;
+        //response code
+         //console.log(data.status);
       }
-    )
+    ),
 
+    error => console.log(error);
   }
+  
 }
 
