98afa432a50162be337cf2f2e5b56e9760928aac
luvnico <ys2k6@mail.missouri.edu>
2018-05-02 22:07:21 -0600

    fixed data retrieval issue of order details component w/ promises, adjusted data representation in dialog

--- src/app/order-detail/order-detail.component.ts
+++ src/app/order-detail/order-detail.component.ts
@@ -3,6 +3,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { HttpClient } from '@angular/common/http';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { Order, CartItem , Product} from '../interfaces';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 @Component({
@@ -12,59 +13,83 @@ import { Order, CartItem , Product} from '../interfaces';
 })
 export class OrderDetailComponent implements OnInit {
   order: Order;
-  products = [];
-  productArray = [];
-
+  products: Product[] = [];
+  productArray: CartItem[] = [];
+  dataSource = new MatTableDataSource<any>();
+  subTotal: number = 0;
 
   constructor(private httpClient:HttpClient, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
-  ngOnInit() {
-    this.httpClient.get(`https://my-json-server.typicode.com/Emilyyan/Shopping-Website/Orders/?orderNumber=${this.data.order_number}`)
-    .subscribe(
-      (orderInfo:any[]) => {
-        if(orderInfo.length) {
-          this.order = orderInfo[0];
-          //console.log(this.order);
-        }
-      },
-      error => console.log("Error", error)
-    ), 
+  displayedColumns = ['name', 'quantity', 'price'];
 
-    this.httpClient.get(`https://my-json-server.typicode.com/Emilyyan/Shopping-Website/Products`)
-    .subscribe(
-      (data:any[]) => {
-        if(data.length){
-          this.products = data;
-          //console.log(this.products);
-        }
-      },
-      error => console.log("Error", error),
-      () => {
+  ngOnInit() {
+
+    this.getOrder(this.data.order_number)
+      .then( data => {
+        this.order = data[0];
+        return this.getProducts();
+      })
+      .then( _ => {
         for(let p of this.order.products){
           let obj = this.getProductDetail(p.pid);
-          let orderitem: CartItem = {
-            pid: p.pid,
-            quantity: p.quantity,
-            name: obj.name,
-            price: obj.price
-          }
-
+          let orderitem: CartItem = { pid: p.pid, quantity: p.quantity, name: obj.name, price: obj.price };
           this.productArray.push(orderitem);
+          this.subTotal += orderitem.quantity * orderitem.price;
         }
-      
-        //console.log(this.productArray);
-        console.log(this.productArray);
-      }
-    )
+        this.dataSource.data = this.productArray;
+      });
   
   }
 
-  private getProductDetail(pid: number){
-    /*for(let i in this.products.id){
-      if(i === pid){
 
-      }
-    }*/
+  /* 
+    Make a GET request to retrieve an order from server
+    Given the order number, would return a promise.
+  */
+  private getOrder(orderNumber: number){
+    let promise = new Promise((resolve, reject) => {
+      let apiURL = `https://my-json-server.typicode.com/Emilyyan/Shopping-Website/Orders?orderNumber=${orderNumber}`;
+      this.httpClient.get(apiURL)
+        .toPromise()
+        .then(
+          data => {
+            resolve(data);
+          },
+          err => {
+            reject();
+          }
+        );
+    });
+    return promise;
+  }
+
+  /* 
+    Make a GET request to retrieve a list of products from server
+    Would return a promise.
+  */
+  private getProducts(){
+    let promise = new Promise((resolve, reject) => {
+      let apiURL = `https://my-json-server.typicode.com/Emilyyan/Shopping-Website/Products`;
+      this.httpClient.get(apiURL)
+        .toPromise()
+        .then(
+          (data:any[]) => {
+            this.products = data;
+            resolve();
+          },
+          err => {
+            reject();
+          }
+        );
+    });
+    return promise;
+  }
+
+  /**
+   * Given the product id,
+   * Would return an object containing all details for that product
+   */
+  private getProductDetail(pid: number){
     let index = this.products.findIndex(x => x.id === pid);
     return this.products[index];
   }
